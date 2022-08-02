167a3beb572f0113f80ad707fabc5f75293d584b
Euan Meston <euan@automata.tech>
2022-08-02 20:59:18 +0100

    Update function finished

--- backend/backend_cli.py
+++ backend/backend_cli.py
@@ -1,16 +1,49 @@
 """Fill this up with all the tools to generate/update databases and queries"""
+import sys
+from sys import argv
+from datetime import datetime
 
-import argparse
+from database_handler import DBHandler
+from database_handler import AustraliaWeightlifting
 
-import database_handler
+
+class CLICommands:
+    """boring shit, will probably realise we don't need this later"""
+    def update(self, db_name):
+        """updates all databases"""
+        match db_name:
+            case "uk":
+                uk_db = DBHandler("https://bwl.sport80.com/", "database_root/UK")
+                uk_db.update_results(datetime.now().year)
+            case "us":
+                us_db = DBHandler("https://usaweightlifting.sport80.com/", "database_root/US")
+                us_db.update_results(datetime.now().year)
+            case "aus":
+                aus_db = AustraliaWeightlifting()
+                aus_db.update_db()
+            case "all":
+                year = datetime.now().year
+                uk_db = DBHandler("https://bwl.sport80.com/", "database_root/UK")
+                uk_db.update_results(year)
+                us_db = DBHandler("https://usaweightlifting.sport80.com/", "database_root/US")
+                us_db.update_results(year)
+                aus_db = AustraliaWeightlifting()
+                aus_db.update_db()
+            case _:
+                sys.exit(f"database not found: {db_name}")
 
 
-def update():
-    """updates all databases"""
+    def refresh_cache(self):
+        """refreshes the query cache"""
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--update", help="updates all root DB's (not query cache)",
-                        action="update")
-    args = parser.parse_args()
+    commands = CLICommands()
+    match argv[1]:
+        case "--update":
+            print(f"updating database: {argv[2]}")
+            commands.update(argv[2])
+        case "--refresh":
+            print("refreshing...")
+        case _:
+            print("not a command")
--- database_handler/__init__.py
+++ database_handler/__init__.py
@@ -3,4 +3,5 @@
 from .handler_main import DBHandler
 from .query_machine import QueryThis
 from .result_dataclasses import Result, DatabaseEntry
+from .web_scrapers import AustraliaWeightlifting
 from .static_helpers import *
