d69bff160f042f7d1cc6dde66181155e3f1a0707
Euan Meston <euan@automata.tech>
2022-12-27 18:54:39 +0000

    Almost there.

--- frontend/components/data-table/AllDetails.jsx
+++ frontend/components/data-table/AllDetails.jsx
@@ -15,13 +15,27 @@ export const AllDetails = ({full_comp}) => {
             <Popover.Content>
                 <Table selectionMode="single" striped aria-label="Open weight lifting lifters extended results table" css={{ tableLayout: 'fixed', textAlign: 'center', width: "auto", paddingLeft: "0", paddingRight: "0" }}>
                     <Table.Header>
-                        <Table.Column css={tableHeaderStyles}>Event</Table.Column>
-                        <Table.Column css={tableHeaderStyles}>Bodyweight</Table.Column>
+                        <Table.Column css={tableHeaderStyles}>Event - {full_comp.event}</Table.Column>
                     </Table.Header>
                     <Table.Body>
                         <Table.Row>
-                            <Table.Cell>{full_comp.event}</Table.Cell>
-                            <Table.Cell>{full_comp.bodyweight}</Table.Cell>
+                            <Table.Cell>Event Date: {full_comp.date}</Table.Cell>
+                        </Table.Row><Table.Row>
+                            <Table.Cell>Bodyweight: {full_comp.bodyweight}</Table.Cell>
+                        </Table.Row><Table.Row>
+                            <Table.Cell>Category: {full_comp.gender}</Table.Cell>
+                        </Table.Row><Table.Row>
+                            <Table.Cell>Opening Snatch: {full_comp.snatch_1}</Table.Cell>
+                        </Table.Row><Table.Row>
+                            <Table.Cell>Second Snatch: {full_comp.snatch_2}</Table.Cell>
+                        </Table.Row><Table.Row>
+                            <Table.Cell>Final Snatch: {full_comp.snatch_3}</Table.Cell>
+                        </Table.Row><Table.Row>
+                            <Table.Cell>Opening C&J: {full_comp.cj_1}</Table.Cell>
+                        </Table.Row><Table.Row>
+                            <Table.Cell>Second C&J: {full_comp.cj_2}</Table.Cell>
+                        </Table.Row><Table.Row>
+                            <Table.Cell>Final C&J: {full_comp.cj_3}</Table.Cell>
                         </Table.Row>
                     </Table.Body>
                 </Table>
