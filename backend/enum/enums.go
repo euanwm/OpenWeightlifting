9b9134ba5ba048294ec3a2a776359c1b94b80dc6
Euan Meston <euan@automata.tech>
2023-01-23 20:49:43 +0000

    Nice easy refactor

--- backend/enum/enums.go
+++ backend/enum/enums.go
@@ -6,18 +6,10 @@ const (
 	Unknown  string = "unknown"
 	Total    string = "total"
 	Sinclair string = "sinclair"
-	//BWL - British Weightlifting
-	BWL string = "UK"
-	//USAW - United States of America Weightlifting
-	USAW string = "US"
-	//IWF - Internation Weightlifting Federation
-	IWF string = "IWF"
-	//ALLFEDS - Pretty self-explanatory
+	// ALLFEDS - Pretty self-explanatory
 	ALLFEDS string = "allfeds"
-	//ALLWEIGHTS - Yes
-	ALLCATS string = "allcats"
-	//AWF - Australian Weightlifting Federation
-	AWF               string  = "AUS"
+	// ALLCATS ALLWEIGHTS - Yes
+	ALLCATS           string  = "allcats"
 	MaxTotal          float32 = 510
 	MinimumBodyweight float32 = 20
 	MaximumBodyweight float32 = 300
