79a327a400e793cf2ff3f31b1f15b1f0cf5fcd86
Euan M <69640923+euanwm@users.noreply.github.com>
2023-02-08 08:24:12 +0000

    Added 4 lifters

--- backend/lifter/instagram.go
+++ backend/lifter/instagram.go
@@ -6,6 +6,10 @@ var instaHandles = map[string]string{
 	"Euan Meston":      "scream_and_jerk",
 	"KRYSTAL CAMPBELL": "da.real.krys",
 	"TALAKHADZE Lasha": "talakhadzelasha_official",
+	"Benedict Millson": "mrbigfriday",
+	"Stefano Cataldi": "stefanotheweightlifter",
+	"Harry Nelms": "13harry1999",
+	"Khrystopher Speed": "kdotspeed",
 }
 
 func CheckUserList(lifterName string) (bool, string) {
