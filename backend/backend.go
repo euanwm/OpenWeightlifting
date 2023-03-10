7fc7f894d4c14453c9b28730e4d3bc3c0f020a52
Euan Meston <euan@automata.tech>
2023-03-10 22:11:10 +0000

    Some aesthetic tweaks

--- backend/backend.go
+++ backend/backend.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"backend/dbtools"
-	"backend/enum"
 	"backend/events"
 	"backend/lifter"
 	"backend/structs"
@@ -62,7 +61,7 @@ func postLifterRecord(c *gin.Context) {
 	}
 }
 
-//Main leaderboard function
+// Main leaderboard function
 func postLeaderboard(c *gin.Context) {
 	body := structs.LeaderboardPayload{}
 	if err := c.BindJSON(&body); err != nil {
@@ -72,21 +71,6 @@ func postLeaderboard(c *gin.Context) {
 	}
 	fedData := dbtools.Filter(processedLeaderboard.Query(body.SortBy, body.Gender), body, dbtools.WeightClassList[body.WeightClass], *lifterData)
 	c.JSON(http.StatusOK, fedData)
-}
-
-func buildDatabase() {
-	log.Println("buildDatabase called...")
-	bigData := dbtools.CollateAll()
-	male, female, _ := dbtools.SortGender(bigData) // Throwaway the unknown genders as they're likely really young kids
-	leaderboardTotal := &structs.LeaderboardData{
-		AllNames:        append(male.ProcessNames(), female.ProcessNames()...),
-		MaleTotals:      dbtools.SortLiftsBy(male.Lifts, enum.Total),
-		FemaleTotals:    dbtools.SortLiftsBy(female.Lifts, enum.Total),
-		MaleSinclairs:   dbtools.SortLiftsBy(male.Lifts, enum.Sinclair),
-		FemaleSinclairs: dbtools.SortLiftsBy(female.Lifts, enum.Sinclair),
-	}
-	processedLeaderboard = *leaderboardTotal
-	log.Println("Database READY")
 }
 
 func CORSConfig(localEnv bool) cors.Config {
@@ -103,22 +87,33 @@ func CORSConfig(localEnv bool) cors.Config {
 	return corsConfig
 }
 
-func main() {
-	go buildDatabase()
-	r := gin.Default()
+func setupCORS(r *gin.Engine) {
 	//It's not a great solution but it'll work
 	if len(os.Args) > 1 && os.Args[1] == "local" {
 		r.Use(cors.New(CORSConfig(true)))
 	} else {
 		r.Use(cors.New(CORSConfig(false)))
 	}
+}
+
+func buildServer() *gin.Engine {
+	log.Println("Starting server...")
+	go dbtools.BuildDatabase()
+	r := gin.Default()
+	setupCORS(r)
 	r.GET("test", getTest)
 	r.POST("leaderboard", postLeaderboard)
 	r.GET("search", getSearchName)
 	r.POST("lifter", postLifterRecord)
 	r.POST("event", postEventResult)
-	err := r.Run()
+	return r
+}
+
+func main() {
+	apiServer := buildServer()
+	err := apiServer.Run()
 	if err != nil {
 		log.Fatal("Failed to run server")
 	}
+	processedLeaderboard = *dbtools.BuildDatabase()
 }
