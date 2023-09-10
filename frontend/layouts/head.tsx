f27de16f6eaba26e1e06fd8e22c05c6cf599e3e4
Euan <euanmeston@gmail.com>
2023-09-10 17:53:37 +0100

    WIP

--- frontend/layouts/head.tsx
+++ frontend/layouts/head.tsx
@@ -1,23 +1,40 @@
 import Image from 'next/image'
-import { Navbar, NavbarBrand, NavbarContent, Switch, Link } from '@nextui-org/react'
+import { Navbar, NavbarBrand, NavbarContent, Switch, Link, NavbarMenuToggle, NavbarMenu, NavbarMenuItem } from "@nextui-org/react";
 import { FaSun, FaMoon, FaGithub, FaInstagram, FaCalculator } from 'react-icons/fa'
 
 import Logo from '../public/OWL-logo.png'
+import { useState } from 'react'
 
 const HeaderBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <Navbar>
-      <title>OpenWeightlifting - The Biggest Weightlifting Database</title>
+    <Navbar isBordered>
+      <title>OpenWeightlifting</title>
       <NavbarBrand>
-        <Image src={Logo} alt='OpenWeightlifting' width={200}/>
+        <Link href="/">
+          <Image src={Logo} alt='OpenWeightlifting' width={130} />
+        </Link>
       </NavbarBrand>
-      <NavbarContent>
-        <Link href="/sinclair"><FaCalculator size='30px' /></Link>
-      </NavbarContent>
-      <a href="https://www.instagram.com/openweightlifting/"><FaInstagram size='30px' /></a>
-      <a href="https://github.com/euanwm/OpenWeightlifting"><FaGithub size='30px' /></a>
+
+      <NavbarMenuToggle
+        aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+      />
+
+      <NavbarMenu>
+        <NavbarMenuItem>
+          <Link href="/sinclair"><FaCalculator size='30px' />Sinclair Calculator</Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <Link href="https://www.instagram.com/openweightlifting/"><FaInstagram size='30px' />Instagram</Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <Link href="https://github.com/euanwm/OpenWeightlifting"><FaGithub size='30px' />GitHub</Link>
+        </NavbarMenuItem>
+      </NavbarMenu>
+
+
         <Switch
-          defaultSelected
           startContent={<FaSun />}
           endContent={<FaMoon />}
         />
