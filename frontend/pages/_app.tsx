fbbb364148d79416ebc827f645094ba9d5801a53
Euan <euanmeston@gmail.com>
2023-09-24 19:00:34 +0100

    Smartlook trial

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,14 +3,13 @@ import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { fontSans, fontMono } from "@/config/fonts";
 import type { AppProps } from "next/app";
+import Smartlook from 'smartlook-client'
 import { useEffect } from "react";
-import { hotjar } from "react-hotjar";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    hotjar.initialize(3147762, 6);
-  }, [])
-
+    Smartlook.init('0cb3d115dd38c136548da8cbdae1e29785f896ac')
+  }, []);
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
