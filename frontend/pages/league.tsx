990ad5ab7bcc92dbdb3a558bd2ba5f3df5e071f1
Bobby Tran <bobbytran@Bobbys-Mac-Studio.local>
2023-10-24 22:39:02 +0100

    259 initial league page styling fixes

--- frontend/pages/league.tsx
+++ frontend/pages/league.tsx
@@ -26,14 +26,17 @@ async function submitForm(data: RegistrationForm): Promise<FormResponse> {
     const response = await fetch('https://v2.openweightlifting.org/register', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
     })
     return response.json()
   } catch (error) {
     console.error('Error:', error)
-    return { success: false, message: 'There was an error submitting your form. Please try again.' }
+    return {
+      success: false,
+      message: 'There was an error submitting your form. Please try again.',
+    }
   }
 }
 
@@ -53,7 +56,7 @@ function validateForm(data: RegistrationForm) {
   if (location.length < 1) {
     return false
   }
-  return checkEmail(email);
+  return checkEmail(email)
 }
 
 function League() {
@@ -71,6 +74,7 @@ function League() {
   const handleSubmit = async (e: any) => {
     e.preventDefault()
     setLoading(true)
+
     if (!validateForm({ username, clubname, location, email, comments })) {
       setError(true)
       setErrorMessage('Please fill out all fields correctly.')
@@ -90,13 +94,14 @@ function League() {
       setDisableButton(true)
       setSubmitted(true)
       setLoading(false)
-    }
-    else if (response.success && response.message === 'You have already registered!') {
+    } else if (
+      response.success &&
+      response.message === 'You have already registered!'
+    ) {
       setDisableButton(true)
       setSubmitted(true)
       setLoading(false)
-    }
-    else {
+    } else {
       setError(true)
       setErrorMessage(response.message)
       setLoading(false)
@@ -106,86 +111,144 @@ function League() {
   return (
     <>
       <HeaderBar />
-      <div className="flex justify-center mt-4">
-        <div className="flex flex-col w-1/2">
-          <h1 className="text-3xl font-bold text-center">The League</h1>
-          <p className="text-center">OpenWeightlifting are pleased to announce that we are now planning our first - club run - league. We aim to run the league during the first 2 weeks of December 2023 and look to run it in a similar format to virtual competitions hosted by other federations.</p>
-          <br/>
-          <h2 className="text-2xl font-bold text-center">How it works</h2>
-          <p className="text-center">The league will be run over 2 weeks, and each registered affiliate will be responsible for recording and uploading their gym /club scores. In a typical competition format, you have 6 attempts in total, 3 for snatch and 3 for clean & jerk. Each attempt will be recorded from the front and the registered club affiliate will be responsible for your weigh-in and logging your scores. You do NOT need to pay for a membership with us or be registered with your National Governing Body.</p>
-          <br/>
-          <h2 className="text-2xl font-bold text-center">The rules</h2>
-          <p className="text-center">Typical weightlifting competition rules apply but we are NOT enforcing a strict press-out rule, a lift will only be disallowed should you exceed approximately 30degrees of elbow flexion. You do not need a singlet, although you can wear one if you think it adds some KGs to your total. Weigh-ins are held by the club affiliate and it is up to yourself how light you want to be on the scale. As always, no straps.</p>
-          <br/>
-          <h2 className="text-2xl font-bold text-center">The cost</h2>
-          <p className="text-center">We are looking to charge around £10GBP per entry which will be invoiced to the club affiliate post-submission of scores and prior to winners being drawn. The breakdown of the fee is as follows, 80% of all fees billed will be put towards the overall prize pot with the remainder going to further development of the OpenWeightlifting League platform and future events.</p>
-          <br/>
-          <h1 className="text-3xl font-bold text-center">League Affiliate Registration</h1>
-          <p className="text-center">Please fill out the form below to register your interest as an affiliate. <i>Reminder, you do not need to hold an NGB affiliation. </i></p>
-          <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
-            <Input
-              aria-label="Username"
-              type="text"
-              placeholder="Username"
-              onChange={e => setUsername(e.target.value)}
-              required
-            />
-            <Input
-              aria-label="Club Name"
-              type="text"
-              placeholder="Club Name"
-              onChange={e => setClubname(e.target.value)}
-              required
-            />
-            <Input
-              aria-label="Location"
-              type="text"
-              placeholder="Location"
-              onChange={e => setLocation(e.target.value)}
-              required
-            />
-            <Input
-              aria-label="Email"
-              type="email"
-              placeholder="Email"
-              onChange={e => setEmail(e.target.value)}
-              required
-            />
-            <Input
-              aria-label="Comments"
-              type="text"
-              placeholder="How many lifters are you looking to enter?"
-              onChange={e => setComments(e.target.value)}
-            />
-            <Popover>
-              <PopoverTrigger>
-            <Button
-              className="flex justify-center"
-              type="submit"
-              aria-label="Submit"
-              color="primary"
-              disabled={disableButton}
+      <div className="flex justify-center mt-4 mb-8">
+        <div className="w-4/5 md:w-2/3 xl:w-1/2 ">
+          <div className="mb-6">
+            <h1 className="text-3xl text-center font-bold mb-2">The League</h1>
+            <p className="text-center">
+              OpenWeightlifting are pleased to announce that we are now planning
+              our first - club run - league. We aim to run the league during the
+              first 2 weeks of December 2023 and look to run it in a similar
+              format to virtual competitions hosted by other federations.
+            </p>
+          </div>
+
+          <div className="mb-6">
+            <h2 className="text-2xl text-center font-bold  mb-2">
+              How it works
+            </h2>
+            <p className="text-center">
+              The league will be run over 2 weeks, and each registered affiliate
+              will be responsible for recording and uploading their gym /club
+              scores. In a typical competition format, you have 6 attempts in
+              total, 3 for snatch and 3 for clean & jerk. Each attempt will be
+              recorded from the front and the registered club affiliate will be
+              responsible for your weigh-in and logging your scores. You do NOT
+              need to pay for a membership with us or be registered with your
+              National Governing Body.
+            </p>
+          </div>
+
+          <div className="mb-6">
+            <h2 className="text-2xl text-center font-bold mb-2">The rules</h2>
+            <p className="text-center">
+              Typical weightlifting competition rules apply but we are NOT
+              enforcing a strict press-out rule, a lift will only be disallowed
+              should you exceed approximately 30degrees of elbow flexion. You do
+              not need a singlet, although you can wear one if you think it adds
+              some KGs to your total. Weigh-ins are held by the club affiliate
+              and it is up to yourself how light you want to be on the scale. As
+              always, no straps.
+            </p>
+          </div>
+
+          <div className="mb-6">
+            <h2 className="text-2xl text-center font-bold mb-2">The cost</h2>
+            <p className="text-center">
+              We are looking to charge around £10GBP per entry which will be
+              invoiced to the club affiliate post-submission of scores and prior
+              to winners being drawn. The breakdown of the fee is as follows,
+              80% of all fees billed will be put towards the overall prize pot
+              with the remainder going to further development of the
+              OpenWeightlifting League platform and future events.
+            </p>
+          </div>
+
+          <div className="mb-6">
+            <h1 className="text-3xl text-center font-bold mb-2">
+              League Affiliate Registration
+            </h1>
+            <p className="text-center">
+              Please fill out the form below to register your interest as an
+              affiliate.
+            </p>
+            <p className="italic text-center">
+              Reminder, you do not need to hold an NGB affiliation.
+            </p>
+          </div>
+
+          {!submitted && (
+            <form
+              className="flex flex-col space-y-4 mt-4"
+              onSubmit={handleSubmit}
             >
-              Submit
-            </Button>
-              </PopoverTrigger>
+              <Input
+                aria-label="Username"
+                type="text"
+                placeholder="Username"
+                onChange={e => setUsername(e.target.value)}
+                required
+              />
+              <Input
+                aria-label="Club Name"
+                type="text"
+                placeholder="Club Name"
+                onChange={e => setClubname(e.target.value)}
+                required
+              />
+              <Input
+                aria-label="Location"
+                type="text"
+                placeholder="Location"
+                onChange={e => setLocation(e.target.value)}
+                required
+              />
+              <Input
+                aria-label="Email"
+                type="email"
+                placeholder="Email"
+                onChange={e => setEmail(e.target.value)}
+                required
+              />
+              <Input
+                aria-label="Comments"
+                type="number"
+                placeholder="How many lifters are you looking to enter?"
+                onChange={e => setComments(e.target.value)}
+              />
+
+              <Popover>
+                <PopoverTrigger>
+                  <Button
+                    className="flex justify-center"
+                    type="submit"
+                    aria-label="Submit"
+                    color="primary"
+                    disabled={disableButton}
+                  >
+                    Submit
+                  </Button>
+                </PopoverTrigger>
+                {error && (
+                  <PopoverContent>
+                    <p className="text-center">{errorMessage}</p>
+                  </PopoverContent>
+                )}
+                {loading && (
+                  <PopoverContent>
+                    <p className="text-center">Loading...</p>
+                  </PopoverContent>
+                )}
+              </Popover>
+            </form>
+          )}
+
           {submitted && (
-            <PopoverContent>
-              <p className="text-center">Thank you for registering your interest. We will be in touch shortly with more information.</p>
-            </PopoverContent>
+            <p className="text-center border-primary border-3 rounded-xl p-4">
+              Thank you for registering your interest. We will be in touch
+              shortly with more information.
+            </p>
           )}
-          {error && (
-            <PopoverContent>
-              <p className="text-center">{errorMessage}</p>
-            </PopoverContent>
-          )}
-          {loading && (
-            <PopoverContent>
-              <p className="text-center">Loading...</p>
-            </PopoverContent>
-          )}
-            </Popover>
-          </form>
         </div>
       </div>
     </>
