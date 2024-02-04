c0420c4c35b36bbe1bc96c6281deda8ced6c3052
Euan <euanmeston@gmail.com>
2024-02-04 17:56:58 +0000

    added remaining endpoint tests

---
+++ frontend/api/fetchLifterNames/fetchLifterNames.test.ts
@@ -0,0 +1,10 @@
+import fetchLifterNames from './fetchLifterNames'
+
+describe('fetchLifterNames', () => {
+  it('should return a list of lifter names', async () => {
+    const result = await fetchLifterNames('Euan Meston');
+
+    expect(result?.names.length).toBe(1)
+    expect(result?.names[0]).toBe('Euan Meston')
+  })
+})
