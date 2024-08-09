4edb7df65d4954dec62d87f0cb139cd6ace9495c
euan <euanmeston@gmail.com>
2024-08-09 07:37:44 -0400

    cache control set to 1 month

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,19 @@ const nextConfig = {
   env: {
     API: process.env.API ?? 'https://api.openweightlifting.org',
   },
+  async headers() {
+    return [
+      {
+        source: '/:all*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=2628000',
+          }
+        ],
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
