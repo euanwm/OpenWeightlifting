b539db27c05ce7441cb92359725cb452ca518f91
euanwm <euanmeston@gmail.com>
2025-02-23 12:32:12 +0000

    query partial caching refactored

--- backend/dbtools/cache_handler.go
+++ backend/dbtools/cache_handler.go
@@ -1,6 +1,7 @@
 package dbtools
 
 import (
+	"backend/enum"
 	"backend/structs"
 	"sync"
 )
@@ -48,11 +49,7 @@ func (q *QueryCache) QueryStatus(query structs.LeaderboardPayload) QueryState {
 	}
 }
 
-// CheckQuery - Checks if the query has been run before, if so, return the data positions.
-// A true value indicates that the query has been run before.
-// A false value indicates that the query has not been run before.
-// And a false value with a non-nil slice indicates that a similar query has been run before and the data positions are returned that should be used to filer from.
-// Hoping that the last case makes things a bit faster to reduce having to run multiple startup caching queries.
+// CheckQuery - Checks if the query has been run before, if so, return the query state and data positions if they exist
 func (q *QueryCache) CheckQuery(query structs.LeaderboardPayload) (QueryState, []int) {
 	loadedData, ok := q.HashStore.Load(query)
 	if ok {
@@ -61,23 +58,30 @@ func (q *QueryCache) CheckQuery(query structs.LeaderboardPayload) (QueryState, [
 			return storedQuery.Status, storedQuery.DataPositions
 		}
 	}
-	/*
-		// if we get here, we haven't found a match, so we'll do some partial matching
-		for _, cacheQuery := range q.Store {
-			// all years for the same total/sinclair, federation and weight class
-			if cacheQuery.Filter.SortBy == query.SortBy && cacheQuery.Filter.Federation == query.Federation && cacheQuery.Filter.WeightClass == query.WeightClass && cacheQuery.Filter.Year == enum.AllYearsStr {
-				return false, cacheQuery.DataPositions
+
+	state := None
+	var positions []int
+	q.HashStore.Range(func(key, value interface{}) bool {
+		loadedQuery, _ := value.(Query)
+		queryPayload, _ := key.(structs.LeaderboardPayload)
+		if queryPayload.SortBy == query.SortBy && queryPayload.Federation == query.Federation && queryPayload.WeightClass == query.WeightClass && queryPayload.Year == enum.AllYearsStr {
+			positions = loadedQuery.DataPositions
+			state = Partial
+			return false
+		}
+		if queryPayload.SortBy == query.SortBy && queryPayload.Federation == query.Federation && queryPayload.Year == enum.AllYearsStr {
+			if query.WeightClass[0] == 'M' && queryPayload.WeightClass == "MALL" {
+				positions = loadedQuery.DataPositions
+				state = Partial
+				return false
 			}
-			// all years for the same total/sinclair, federation, and all gendered weight classes
-			if cacheQuery.Filter.SortBy == query.SortBy && cacheQuery.Filter.Federation == query.Federation && cacheQuery.Filter.Year == enum.AllYearsStr {
-				if query.WeightClass[0] == 'M' && cacheQuery.Filter.WeightClass == "MALL" {
-					return false, cacheQuery.DataPositions
-				}
-				if query.WeightClass[0] == 'F' && cacheQuery.Filter.WeightClass == "FALL" {
-					return false, cacheQuery.DataPositions
-				}
+			if query.WeightClass[0] == 'F' && queryPayload.WeightClass == "FALL" {
+				positions = loadedQuery.DataPositions
+				state = Partial
+				return false
 			}
 		}
-	*/
-	return None, nil
+		return false
+	})
+	return state, positions
 }
