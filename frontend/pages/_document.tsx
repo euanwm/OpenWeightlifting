41e4a2a5c9892922f16efe2ba1e4f5b431ee2cb8
euanwm <euanmeston@gmail.com>
2025-03-01 19:02:47 +0000

    widget fix attempt 3

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -6,9 +6,9 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <title>OpenWeightlifting</title>
-        <body className="min-h-screen bg-background font-sans antialiased">
-        <Script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="openweightlifting" data-description="Support us on Buy me a coffee!" data-message="" data-color="#00B0F0" data-position="Right" data-x_margin="18" data-y_margin="18" />
-        <Main />
+      <body className="min-h-screen bg-background font-sans antialiased">
+      <Main />
+      <Script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="openweightlifting" data-description="Support us on Buy me a coffee!" data-message="" data-color="#00B0F0" data-position="Right" data-x_margin="18" data-y_margin="18" />
       <NextScript />
       </body>
     </Html>
